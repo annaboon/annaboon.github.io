e9208b8ef6cd11ce364ca26a52bde32ed583a33a
Boon <aboon@allseas.com>
2025-06-25 13:59:07 +0200

    update rsvp

--- js/scripts.js
+++ js/scripts.js
@@ -184,28 +184,27 @@ $(document).ready(function () {
 
 
     /********************** RSVP **********************/
-    $('#rsvp-form').on('submit', function (e) {
-        e.preventDefault();
-        var data = $(this).serialize();
 
-        $('#alert-wrapper').html(alert_markup('info', '<strong>Even geduld</strong> We zijn de informatie aan het opslaan.'));
+$("#rsvp-form").on("submit", function(e) {
+  e.preventDefault();
+  var t = $(this).serialize();  // Ensure 't' is defined here
+  $("#alert-wrapper").html(alert_markup("info", "<strong>Even geduld</strong> We zijn de informatie aan het opslaan."));
 
-		$.post("https://script.google.com/macros/s/AKfycbwivWX4iEaYhZMfHhpHFUDqumcZMeuvYZjz5CC4bKP3VZrE4Fgp4fgeAa6BDw1rAUMu/exec", t)
-		  .done(function(e) {
-			console.log(e);
-			if (e.result === "error") {
-			  $("#alert-wrapper").html(alert_markup("danger", e.message));
-			} else {
-			  $("#alert-wrapper").html("");
-			  $("#rsvp-modal").modal("show");
-			}
-		  })
-		  .fail(function(e) {
-			console.log(e);
-			$("#alert-wrapper").html(alert_markup("danger", "<strong>Sorry!</strong> Er is een probleem met de server."));
-		  });
-
+  $.post("https://script.google.com/macros/s/AKfycbz5KiufWLBc811RH81PyvB_y8rRKtY9cHG_ickoMOgOo8OUonAUjhYuRkrz8KMWunLE/exec", t)
+    .done(function(e) {
+      console.log(e);
+      if (e.result === "error") {
+        $("#alert-wrapper").html(alert_markup("danger", e.message));
+      } else {
+        $("#alert-wrapper").html("");
+        $("#rsvp-modal").modal("show");
+      }
+    })
+    .fail(function(e) {
+      console.log(e);
+      $("#alert-wrapper").html(alert_markup("danger", "<strong>Sorry!</strong> Er is een probleem met de server."));
     });
+});
 
 });
 
